5c9cae7f3cf5494e3b5e90084453ad8545e29695
Juan-Koder <stacruz2k@hotmail.com>
2021-11-08 13:44:30 -0600

    Linux test

--- suma.c
+++ suma.c
@@ -4,8 +4,9 @@ int main(){
 	int a, b;
 	printf ("Ingrese 2 numeros enteros: \n");
 	scanf ("%d %d", &a, &b);
-	printf ("La suma es: %d", a+b);
-	return 0; //return 0
+	printf ("La suma es: %d\n", a+b);
+	printf ("La resta es: %d\n", a-b);
+	return 0;
 }
 
 // https://github.com/Juan-Koder/C-Basics.git
