cd29e9b61208e02c5f8c8b39ec75c14b168f6804
Juan-Koder <stacruz2k@hotmail.com>
2021-11-11 13:50:41 -0600

    HW2

--- C_HW/ex01.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-// Impresion Basica en Lenguage C
-int main(int args, char *argv[]){
-	int distance = 100;
-	printf ("You are %d miles away.\n", distance);
-	return 0; //Regresa cero
-}
