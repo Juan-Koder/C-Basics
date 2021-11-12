6706bdb57961dbe9502e83ed00fba03e36655970
Juan-Koder <stacruz2k@hotmail.com>
2021-11-12 10:19:08 -0600

    resync

---
+++ C_HWay/ex1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+// Impresion Basica en Lenguage C
+int main(int args, char *argv[]){
+	int distance = 100;
+	printf ("You are %d miles away.\n", distance);
+	return 0; //Regresa cero
+}
